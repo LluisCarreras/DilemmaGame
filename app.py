407715aecd6ca9cd843647f8b511bc841a53556b
LluisCarreras <lluissot@gmail.com>
2025-01-16 16:52:45 +0100

    Add turn column to table in HTML

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 #number = random.randint(1, 100)
 
 turn = 0
+player1_score = 0
+player2_score = 0
+
+def create_json (message, turn, value_p1, value_p2):
+    return jsonify({'message': message,
+                    'value_turn': turn,
+                    'value_player_1': value_p1,
+                    'value_player_2': value_p2})
 
 @app.route('/')
 def index():
@@ -16,18 +24,20 @@ def index():
 @app.route('/guess', methods=['POST'])
 def guess():
     global turn
+    global player1_score
+    global player2_score
     does_cooperate = int(request.json['guess'])  
     turn += 1 
     if does_cooperate: 
-        return jsonify({'message': 'Cooperates!!!',
-                        'value_turn': turn,
-                        'value_player_1': 1,
-                        'value_player_2': 0})
+        message = 'Cooperates!!!'
+        value_p1 = 1
+        value_p2 = 0
+        return create_json(message, turn, value_p1, value_p2)
     else:
-        return jsonify({'message': 'Defects!',
-                        'value_turn': turn,
-                        'value_player_1': 0,
-                        'value_player_2': 1})
+        message = 'Defects!'
+        value_p1 = 0
+        value_p2 = 1
+        return create_json(message, turn, value_p1, value_p2)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
