4350985b72f9fd18e15829b37731fd798556f59a
LluisCarreras <lluissot@gmail.com>
2025-01-16 16:43:49 +0100

    Add table to HTML

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request, jsonify
 import random
+from flask import Flask, render_template, request, jsonify
+
 
 app = Flask(__name__)
 
 # Generate the number to guess
-number = random.randint(1, 100)
+#number = random.randint(1, 100)
+
+turn = 0
 
 @app.route('/')
 def index():
@@ -12,15 +15,19 @@ def index():
 
 @app.route('/guess', methods=['POST'])
 def guess():
-    global number
-    guess = int(request.json['guess'])  # Get the guess from the client
-    if guess < number:
-        return jsonify({'message': 'Too low!'})
-    elif guess > number:
-        return jsonify({'message': 'Too high!'})
+    global turn
+    does_cooperate = int(request.json['guess'])  
+    turn += 1 
+    if does_cooperate: 
+        return jsonify({'message': 'Cooperates!!!',
+                        'value_turn': turn,
+                        'value_player_1': 1,
+                        'value_player_2': 0})
     else:
-        number = random.randint(1, 100)  # Reset the number for a new game
-        return jsonify({'message': 'Congratulations! You guessed it!'})
+        return jsonify({'message': 'Defects!',
+                        'value_turn': turn,
+                        'value_player_1': 0,
+                        'value_player_2': 1})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
